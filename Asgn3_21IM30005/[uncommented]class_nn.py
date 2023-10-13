ecf663ba90c70f90e3667a045e366a15a89f7b43
ayush4ise <pkumar46802@gmail.com>
2023-10-13 19:28:30 +0530

    finalized code

---
+++ Asgn3_21IM30005/[uncommented]class_nn.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+class NN():
+    def __init__(self, input_layer_size = 2, hidden_layer_size = 2, output_layer_size = 2, hidden_layers = 1, lr = 0.1, epoches = 1000):
+        self.input_layer_size = input_layer_size
+        self.hidden_layer_size = hidden_layer_size
+        self.output_layer_size = output_layer_size
+        self.hidden_layers = hidden_layers
+        self.lr = lr
+        self.number_of_epoches = epoches
+
+        # initialize the weights by random numbers
+        self.W = [np.random.randn(self.input_layer_size, self.hidden_layer_size)]
+        for layer in range(hidden_layers-1):
+            self.W.append(np.random.randn(self.hidden_layer_size, self.hidden_layer_size))
+        self.W.append(np.random.randn(self.hidden_layer_size, self.output_layer_size))
+
+    def activation(self, x): # sigmoid function
+        return (1/(1+np.exp(-x)))
+    
+    def forward(self, X, less_steps = False): # forward prapogation
+        # X- input
+        input_list = X
+
+        if less_steps == False: # for full forwward propagation to get final output
+            less_steps = self.hidden_layers + 1 
+
+        # if a value is given to "steps" parameter, then we perform forward propagation to that step 
+        for step in range(less_steps):
+            output_list = np.dot(input_list, self.W[step])
+            output_list = [self.activation(neuron) for neuron in output_list]
+            input_list = output_list
+        return input_list # since input_list = output_list at the end of the iterations and also we can use it for our loop in backward()
+    
+    def backward(self, X, y): # backward propagation
+        # X- input, y- output
+        # output_list = self.forward(X)
+        # total error = np.sum((y - output_list)**2/2)
+        hidden_layers = self.hidden_layers
+        # first back propagation for last layer
+        # error = (output)(1-output)(actual-output)
+        layer_error = self.forward(X) * (np.array(1) - self.forward(X)) * (np.array(y) - self.forward(X))
+        # Weight updation- W = W + alpha * (H * ((y-OP) * (OP * (1 - OP))))
+        self.W[-1] += self.lr * (np.dot(np.matrix(self.forward(X,hidden_layers)).transpose(), layer_error).transpose())
+
+        # there are hidden_layers + 1 number of weight matrices, hidden_layers number of matrices if we calculate first one out of loop
+        for step in range(2,hidden_layers+2): # the loop runs for hiden_layers number of iterations
+            layer_error = np.array(self.forward(X, hidden_layers+2-step)) * (np.array(1) - np.array(self.forward(X, hidden_layers+2-step))) * np.dot(self.W[-step+1],np.matrix(layer_error).transpose())
+            self.W[-step] += self.lr * (np.dot(np.matrix(self.forward(X,hidden_layers+1-step)).transpose(), layer_error).transpose())
+
+    # training: update the weights for the given number of epoches
+    def train(self, X, y):
+        for _ in range(self.number_of_epoches):
+            self.backward(X, y)
+    
+    def total_error(self,X,y):
+        return np.sum((y - self.forward(X))**2/2)  
--- Asgn3_21IM30005/class_nn.py
+++ Asgn3_21IM30005/class_nn.py
@@ -1,58 +1,116 @@
 import numpy as np
 
 class NN():
-    def __init__(self, input_layer_size = 2, hidden_layer_size = 2, output_layer_size = 2, hidden_layers = 1, lr = 0.1, epoches = 1000):
+    def __init__(self, input_layer_size=2, hidden_layer_size=2, output_layer_size=2, hidden_layers=1, lr=0.1, epochs=1000):
+        """
+        Initialize a neural network with specified parameters.
+
+        Args:
+            input_layer_size (int): Number of input neurons.
+            hidden_layer_size (int): Number of neurons in each hidden layer.
+            output_layer_size (int): Number of output neurons.
+            hidden_layers (int): Number of hidden layers.
+            lr (float): Learning rate for weight updates.
+            epochs (int): Number of training epochs.
+        """
+        # Initialize network parameters
         self.input_layer_size = input_layer_size
         self.hidden_layer_size = hidden_layer_size
         self.output_layer_size = output_layer_size
         self.hidden_layers = hidden_layers
         self.lr = lr
-        self.number_of_epoches = epoches
+        self.number_of_epochs = epochs
 
-        # initialize the weights by random numbers
+        # Initialize weights randomly
         self.W = [np.random.randn(self.input_layer_size, self.hidden_layer_size)]
         for layer in range(hidden_layers-1):
             self.W.append(np.random.randn(self.hidden_layer_size, self.hidden_layer_size))
         self.W.append(np.random.randn(self.hidden_layer_size, self.output_layer_size))
 
-    def activation(self, x): # sigmoid function
-        return (1/(1+np.exp(-x)))
+    def activation(self, x):
+        """
+        Sigmoid activation function.
+
+        Args:
+            x (float): Input value.
+
+        Returns:
+            float: Activated value.
+        """
+        return 1 / (1 + np.exp(-x))
     
-    def forward(self, X, less_steps = False): # forward prapogation
-        # X- input
+    def forward(self, X, less_steps=False):
+        """
+        Perform forward propagation.
+
+        Args:
+            X (numpy.ndarray): Input data with shape (num_samples, num_features).
+            less_steps (bool, optional): If True, perform partial forward propagation.
+
+        Returns:
+            numpy.ndarray: Output of the network with shape (num_samples, output_layer_size).
+        """
+        num_samples = X.shape[0]
         input_list = X
 
-        if less_steps == False: # for full forwward propagation to get final output
+        if not less_steps:
             less_steps = self.hidden_layers + 1 
-        # if a value is given to "steps" parameter, then we perform forward propagation to that step 
+
+        outputs = np.zeros((num_samples, self.output_layer_size))
 
         for step in range(less_steps):
             output_list = np.dot(input_list, self.W[step])
             output_list = [self.activation(neuron) for neuron in output_list]
             input_list = output_list
-        return input_list # since input_list = output_list at the end of the iterations and also we can use it for our loop in backward()
+            outputs = output_list
+
+        return outputs
+
     
-    def backward(self, X, y): # backward propagation
-        # X- input, y- output
-        # output_list = self.forward(X)
-        # total error = np.sum((y - output_list)**2/2)
-        hidden_layers = self.hidden_layers
-        # first back propagation for last layer
-        # error = (output)(1-output)(actual-output)
-        layer_error = np.array(self.forward(X)) * (1 - np.array(self.forward(X))) * (np.array(y) - np.array(self.forward(X)))
-        # Weight updation- W = W + alpha * (H * ((y-OP) * (OP * (1 - OP))))
-        self.W[-1] += self.lr * (np.dot(np.matrix(self.forward(X,hidden_layers)[0]).transpose(), layer_error))
+    def backward(self, X, y):
+        """
+        Perform backward propagation.
 
-        # there are hidden_layers + 1 number of weight matrices, hidden_layers - 1 if we calculate first and last one out of loop
-        for step in range(2,hidden_layers+2):
-            layer_error = np.array(self.forward(X, hidden_layers+2-step)) * (1 - np.array(self.forward(X, hidden_layers+2-step))) * np.dot(self.W[-step+1],np.matrix(layer_error).transpose())
-            self.W[-step] += self.lr * (np.dot(np.matrix(self.forward(X,hidden_layers+1-step)[0]).transpose(), layer_error))
+        Args:
+            X (numpy.ndarray): Input data with shape (num_samples, num_features).
+            y (numpy.ndarray): Target output with shape (num_samples, output_layer_size).
 
+        Returns:
+            None
+        """
+        num_samples = X.shape[0]
 
-    # training: update the weights for the given number of epoches
+        for sample in range(num_samples):
+            layer_error = self.forward(X[sample]) * (np.array(1) - self.forward(X[sample])) * (y[sample] - self.forward(X[sample]))
+            self.W[-1] += self.lr * (np.dot(np.matrix(self.forward(X[sample])).transpose(), layer_error).transpose())
+
+            for step in range(2, self.hidden_layers + 2):
+                layer_error = np.array(self.forward(X[sample], self.hidden_layers+2-step)) * (np.array(1) - np.array(self.forward(X[sample], self.hidden_layers+2-step))) * np.dot(self.W[-step+1], np.matrix(layer_error).transpose())
+                self.W[-step] += self.lr * (np.dot(np.matrix(self.forward(X[sample], self.hidden_layers+1-step)).transpose(), layer_error).transpose())
+
     def train(self, X, y):
-        for _ in range(self.number_of_epoches):
+        """
+        Train the neural network.
+
+        Args:
+            X (numpy.ndarray): Input data.
+            y (numpy.ndarray): Target output.
+
+        Returns:
+            None
+        """
+        for _ in range(self.number_of_epochs):
             self.backward(X, y)
     
-    def total_error(self,X,y):
-        return np.sum((y - self.forward(X))**2/2)  
+    def total_error(self, X, y):
+        """
+        Calculate the total error of the network.
+
+        Args:
+            X (numpy.ndarray): Input data.
+            y (numpy.ndarray): Target output.
+
+        Returns:
+            float: Total error.
+        """
+        return np.sum((y - self.forward(X))**2 / 2)
